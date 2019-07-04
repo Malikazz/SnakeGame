f0e7c0212ac8797b108a3742ab2dda635524979a
Paul Mana <paul@malikaz.com>
2019-07-03 18:50:00 -0600

    a

--- Program.cs
+++ Program.cs
@@ -7,8 +7,7 @@ namespace SnakeGame
         
         static void Main(string[] args)
         {
-           Snake[] snakes = new Snake[200];
-           Food food = new Food(10f,10f,SimpleWindow.WINDOW);
+
            var window = new SimpleWindow();
            window.Run();
 
