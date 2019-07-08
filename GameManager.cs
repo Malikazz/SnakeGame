65f8f51df0d31790bdc8a550e70c168c210128c0
Paul Mana <paul@malikaz.com>
2019-07-08 09:35:10 -0600

    g

---
+++ GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFML.System;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace SnakeGame
+{
+    class GameManager
+    {
+
+    }
+}
